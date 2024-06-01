b70f9ed371f14a6defdacde70e380f0237c4270e
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-06-01 14:54:52 +0400

    change location of middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,6 +10,6 @@ export function middleware(request: NextRequest) {
   return I18nMiddleware(request);
 }
 
-export const config = {
-  matcher: ["/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)"],
-};
+// export const config = {
+//   matcher: ["/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)"],
+// };
